7e809c947675267d3f4072947b0b6e2a2e72d94b
vuongkykhan <vuongkykhan@gmail.com>
2018-06-01 16:58:15 +0700

    update_quantity_of_product_in_add_cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,21 +12,34 @@ class CartsController < ApplicationController
     else
       session[:cart][@product.id.to_s] = Settings.quantity.default
     end
-      respond_to do |format|
-        format.html { render "static_pages/home" }
-        format.json {}
-        format.js
-      end
+    update_decrement_product_quantity Settings.quantity.default
+    respond_to do |format|
+      format.html { render "static_pages/home" }
+      format.json {}
+      format.js
+    end
   end
 
   def remove_cart
-    session[:cart].delete @product.id.to_s if session[:cart].key?(@product.id.to_s)
+    if session[:cart].key?(@product.id.to_s)
+      update_increment_product_quantity session[:cart][@product.id.to_s]
+      session[:cart].delete @product.id.to_s
+    end
     redirect_to carts_url
   end
 
   def update_hard_cart
     return render_validate_fail unless validate_quantity_cart @product, params[:quantity].to_i, true
-    session[:cart][@product.id.to_s] = params[:quantity].to_i if session[:cart].key?(@product.id.to_s)
+    return return_quantity_is_zero if params[:quantity].to_i < Settings.quantity.default
+    if session[:cart].key?(@product.id.to_s)
+      current_quantity = session[:cart][@product.id.to_s]
+      if current_quantity > params[:quantity].to_i
+        update_increment_product_quantity current_quantity - params[:quantity].to_i
+      elsif current_quantity < params[:quantity].to_i
+        update_decrement_product_quantity params[:quantity].to_i - current_quantity
+      end
+      session[:cart][@product.id.to_s] = params[:quantity].to_i
+    end
     redirect_to carts_url
   end
 
@@ -45,8 +58,21 @@ class CartsController < ApplicationController
     redirect_to carts_url
   end
 
+  def return_quantity_is_zero
+    flash[:danger] = t ".validate_quantity_is_zero"
+    redirect_to carts_url
+  end
+
   def find_product
     @product = Product.find_by id: params[:id_product]
     valid_object @product
+  end
+
+  def update_decrement_product_quantity quantity
+    @product.decrement!(:quantity, quantity)
+  end
+
+  def update_increment_product_quantity quantity
+    @product.increment!(:quantity, quantity)
   end
 end
